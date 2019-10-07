c3f602926819f2dbe289dea1cea5d9bc5df5e116
ArturVargas <arturvargas9226@gmail.com>
2019-10-07 16:36:53 -0500

    [add] creando Tareas

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,4 +4,21 @@ pragma solidity ^0.5.0;
 contract TodoList {
     uint public taskCount = 0;
 
+//Definimos una estructura para las tareas.
+// Las estructuras 
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Mi First Dapp");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
